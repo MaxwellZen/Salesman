ab83ba60a708326066deb0458a292e1dfefd3d33
mmelucci <mmelucci20@stuy.edu>
2021-01-19 21:18:59 -0500

    Added explanation of adj data structure

--- salesman.java
+++ salesman.java
@@ -7,44 +7,42 @@ public class salesman {
           //file reading
 
           ArrayList<String> cities = new ArrayList();
-          int adj[][] = new int[10][10];
+          int adj[][] = new int[10][10]; // this 2-D array will contain the distances between adjacent
+                                         // cities. It has as many columns and rows as the total
+                                         // number of cities. We've been told that there's a maximum of
+                                         // ten cities, which is why we have declared the array as 10x10
           Scanner s = new Scanner(System.in);
           while (s.hasNext()) {
-               String line = s.nextLine(); //would look like "Faerun to Norrath = 129"
+               String line = s.nextLine();
                Scanner l = new Scanner(line);
-               String originCity = l.next();
+               String c = l.next();
                //if the city isn't there, add it
-               if (cities.indexOf(originCity)==-1) {
-                    cities.add(originCity);
+               if (cities.indexOf(c)==-1) {
+                    cities.add(c);
                }
-               l.next(); //skip the word "to"
-               String destinationCity = l.next();
+               l.next();
+               String d = l.next();
                //if the city isn't there, add it
-               if (cities.indexOf(destinationCity)==-1) {
-                    cities.add(destinationCity);
+               if (cities.indexOf(d)==-1) {
+                    cities.add(d);
                }
-               l.next(); //skip the equals sign
+               l.next();
                int dist = l.nextInt();
                //set the distances in adj
-               adj[cities.indexOf(originCity)][cities.indexOf(destinationCity)]=dist;
-               adj[cities.indexOf(destinationCity)][cities.indexOf(originCity)]=dist;
+               adj[cities.indexOf(c)][cities.indexOf(d)]=dist;
+               adj[cities.indexOf(d)][cities.indexOf(c)]=dist;
 
-          } 
-              //sample final cities list would look like: [London, Dublin, Belfast]
-              //final adjacency list would look like:         L   D    B
-              //                                         [ L [0, 464, 518],             [ [L to L, L to D, L to B],
-              //                                           D [464, 0, 141],      =>       [D to L, D to D, D to B],
-              //                                           B [518, 141, 0] ]              [B to L, B to D, B to B] ]
+          }
 
           //calculate stuff
           // assign each permutation a number and loop through those numbers
           int n = cities.size();
           int fact = 1;
           //calculate n-1 factorial
-          for (int i = 2; i < n; i++) {
+          for (int i = 1; i < n; i++) {
                fact *= i;
           }
-          int min = Integer.MAX_VALUE; //an arbitrary value so that the shortest path can be found
+          int min = 10000;
           ArrayList<Integer> remain = new ArrayList(n);
           for (int pp = 0; pp < fact; pp++) {
                int p = pp;
@@ -62,13 +60,13 @@ public class salesman {
                     p /= i;
                }
                //NOTES ON WHY THE ABOVE LOOP WORKS:
-                    /* So first it's important to note that to generate a path of size n, it is sufficient to generate
-                     a sequence of numbers where the first number is from 0 to n-1, second number from 0 to n-2,
-                     etc until the last number is 0. Reason why being each number will determine which of the
-                     remaining cities will be next in the permutation. The above code does that by taking
-                     p mod i, where i decreases from n-1 to 1 (generating a path n-1 cities long). The order in which
-                     the permutations appear is irrelevant, all that matters is that each value of p gives a unique
-                     path, which can be shown since each step taken is reversible.*/
+                    // So first it's important to note that to generate a path of size n, it is sufficient to generate
+                    // a sequence of numbers where the first number is from 0 to n-1, second number from 0 to n-2,
+                    // etc until the last number is 0. Reason why being each number will determine which of the
+                    // remaining cities will be next in the permutation. The above code does that by taking
+                    // p mod i, where i decreases from n-1 to 1 (generating a path n-1 cities long). The order in which
+                    // the permutations appear is irrelevant, all that matters is that each value of p gives a unique
+                    // path, which can be shown since each step taken is reversible.
                // calculate the sum and the maximum edge
                int currentsum = 0;
                int cmax = 0;
@@ -77,16 +75,16 @@ public class salesman {
                     currentsum += dist;
                     cmax = Math.max(dist, cmax);
                }
-               int dist = adj[path.get(0)][path.get(n-1)]; //we do this line because of the condition in the for loop above 
+               int dist = adj[path.get(0)][path.get(n-1)];
                currentsum += dist;
                cmax = Math.max(dist, cmax);
 
                // the best path within that loop will be the total loop sum minus the longest edge
                min = Math.min(currentsum-cmax, min);
           }
-          System.out.println("Minimum distance: " + min);
+          System.out.println(min);
           double time = (System.currentTimeMillis()-start)/1000.0;
-		System.out.println("Runtime: " + time);
+		System.out.println(time);
      }
 
 }
